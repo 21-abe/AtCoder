5c261601f55e6c8d3a9848d00fb00e3fb810d243
阿部真之 <93029401+matcha-am@users.noreply.github.com>
2025-05-26 11:14:24 +0900

    Delete AtCoder Regular Contest/018 directory

--- AtCoder Regular Contest/018/A - BMI.cpp
+++
@@ -1,9 +0,0 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-int main() {
-  double Height = 0;
-  double BMI = 0;
-  cin >> Height >> BMI;
-  cout << (Height/100) * (Height/100) * BMI << endl;
-}
